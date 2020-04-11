fb060ccad0b08484c0dec5ac0683a23b74f4b13a
Vivek Mahajan <vivek.mahajan@iag.com.au>
2020-04-11 13:46:45 +1000

    Added Dependency Injection for Property, Setter and Constructor injected Controllers and Greeting Service.

--- src/main/java/example/spring/demo/SpringDemoApplication.java
+++ src/main/java/example/spring/demo/SpringDemoApplication.java
@@ -1,6 +1,9 @@
 package example.spring.demo;
 
+import example.spring.demo.controllers.ConstructorInjectedController;
 import example.spring.demo.controllers.MyController;
+import example.spring.demo.controllers.PropertyInjectedController;
+import example.spring.demo.controllers.SetterInjectedController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -13,6 +16,17 @@ public class SpringDemoApplication {
 		MyController myController = (MyController) ctx.getBean("myController");
 		System.out.println(myController.sayHello());
 
+		System.out.println("------------ Property");
+		PropertyInjectedController propertyInjectedController = (PropertyInjectedController) ctx.getBean("propertyInjectedController");
+		System.out.println(propertyInjectedController.getGreeting());
+
+		System.out.println("------------ Setter");
+		SetterInjectedController setterInjectedController = (SetterInjectedController) ctx.getBean("setterInjectedController");
+		System.out.println(setterInjectedController.getGreeting());
+
+		System.out.println("------------ Constructor");
+		ConstructorInjectedController constructorInjectedController = (ConstructorInjectedController) ctx.getBean("constructorInjectedController");
+		System.out.println(constructorInjectedController.getGreeting());
 	}
 
 }
